7840e1b78f156c27bdeccc84f0faed426c67c747
Stanislav18s <polet18stones@gmail.com>
2025-04-22 23:50:24 +0500

    создал пару функций 1 вывод сообщений 2 умножение

--- workout.cpp
+++ workout.cpp
@@ -1,10 +1,20 @@
 ﻿
-
+#include <stdio.h>
 #include <iostream>
 using namespace std;
+// тренируюсь создовать новые методы
+void char_messege(const char array[])
+{
+    cout << array << "\n" << endl;
+}
+// создал функцию умножения
+float multiply(float x, float y) {
+    float temp = x * y;
+    return temp;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char_messege("kak dela?");
 }
 
