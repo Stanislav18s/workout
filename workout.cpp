3ff1f05055661ebb322245566e00acdecaee6d14
Stanislav18s <polet18stones@gmail.com>
2025-04-23 15:06:24 +0500

    изучаем рекурсии  в функциях

--- workout.cpp
+++ workout.cpp
@@ -10,11 +10,29 @@ void char_messege(const char array[])// здесь нужна констанка
 // создал функцию умножения
 float multiply(float x, float y) {
     float temp = x * y;
-    return temp;
+    return temp;// Без равно можно написать выражение return x*2;
 }
+// рекурсивная функция( матрешка)
+int recursiv(int i) {
+    if (i == 1) return 1;
+    return i + recursiv(i-1);// нужно что бы функция упала в первоначальное положение без этой функции 
+
+   
+   
+    return recursiv(i);
+
+    }// функция которая дает сумму всех натуральных чисел i 
+  
+
+
 
 int main()
 {
+  int i = 4;
+  recursiv(i);
+
+  printf("%d\n", recursiv(i));
     char_messege("kak dela?");
+    return 0;
 }
 
