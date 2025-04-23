ada517e22c2b89e5fedd4dfabc1dcac3967b8057
Stanislav18s <polet18stones@gmail.com>
2025-04-23 17:58:21 +0500

    array  parametr  in metod

--- workout.cpp
+++ workout.cpp
@@ -5,18 +5,25 @@
 using namespace std;
 // отрабатывем передачу параметров в функцию указатели 
 
-// переработал функцию заменны на ссылки сама функция не может изменить значение переменной а ссылка может
-
+// массивы в функциях 
+void aray(size_t x, int arr[]) {
+    for (size_t i = 0; i < x; i++) {
+        arr[i] += 5;
+   }
+}
 
 
 
 
 int main()
 {
-    int b = 12;
-    int c = 30;
-    zamena(&b, &c);
-    cout << b<<"\t"<< c << "\n" << endl;
+    int ar[] = { 1,2,3,4,5,6 };
+    size_t x = sizeof(ar) / sizeof(ar[0]);
+    aray(x, ar);
+    for (size_t i=0;i < x;i++)
+    {
+        cout << ar[i] << "\n" << endl;
+    }
     return 0;
 }
 
