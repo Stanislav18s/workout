748639b9558add2822499a6c145a1831d2c47b19
Stanislav18s <polet18stones@gmail.com>
2025-04-24 23:09:57 +0500

    закрашил программу из за неправильного размера который передал в qsort но deepsek помог

--- workout.cpp
+++ workout.cpp
@@ -16,15 +16,23 @@
 Подсказка: Используйте strcmp из <string.h>.*/
 using namespace std;
 
+int compare_string(const void* a, const void* b)
+{
 
-
-
+      return strcmp(*(const char**)b, *(const char**)a);
 }
+
+
+
 int main()
 
 {
-   
-  
-    return 0;
+   const char *names[] = {"Alisa","Bob","Charli","David"};
+   size_t n = sizeof(names) / sizeof(names[0]);
+   qsort(names, n, sizeof(const char*), compare_string);
+   for (size_t i = 0;i < n;i++) {
+       printf("%s\n", names[i]);
+   }
+      return 0;
 }
 
