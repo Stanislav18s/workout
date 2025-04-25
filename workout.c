e94395bfc39d1c64556c14f78eb2104347ea452e
Stanislav18s <polet18stones@gmail.com>
2025-04-25 17:10:04 +0500

    указатель на указатель ебанный массив указателей , разименуем потом разименуем хуйня по сортировки   структуры ааааааааааа тоже указатели массивы как бы указатели обьекты тоже есть указатель.

---
+++ workout.c
@@ -0,0 +1,36 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Person {
+    char* name;
+    int age;
+} Person;
+
+int main() {
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+
+    Person* mas[] = {
+        &(Person) { "Silver", rand() % 50 },
+        &(Person) {
+"Tom",    rand() % 40
+},
+&(Person) {
+"Bob",    rand() % 40
+},
+&(Person) {
+"Alice",  rand() % 40
+},
+&(Person) {
+"Sam",    rand() % 40
+}
+    };
+
+    // Проверка
+    for (int i = 0; i < 5; i++) {
+        printf("%s: %d\n", mas[i]->name, mas[i]->age);
+    }
+
+    return 0;
+}
--- workout.cpp
+++ workout.cpp
@@ -18,33 +18,40 @@ struct Person {
   int age;
 };
 int compare_age(const void* a, const void* b) {
-    const Person* p1 = (const Person*)a;
-    const Person* p2 = (const Person*)b;
+    const Person* p1 = *(const Person**)a;
+    const Person* p2 = *(const Person**)b;
     return p1->age - p2->age;
 }
 int compare_name(const void* a, const void* b) {
-    const Person* p1 = (const Person*)a;
-    const Person* p2 = (const Person*)b;
+    const Person* p1 = *(const Person**)a;
+    const Person* p2 = *(const Person**)b;
     return strcmp(p1->name, p2->name);
 }
 
 int main()
 {
     srand(time(NULL));
-    struct Person mas[] = { {"Silver",rand() % 50}, { "Tom",rand() % 40 }, {"Bob",rand() % 40}, {"Alice",rand() % 40}, {"Sam",rand() % 40} };
+    struct Person *mas[] = { 
+       new Person {"Silver",rand() % 50},
+       new Person  { "Tom",rand() % 40 },
+       new Person  {"Bob",rand() % 40},
+       new Person  {"Alice",rand() % 40},
+        new Person {"Sam",rand() % 40}
+    };
     size_t n = sizeof(mas) / sizeof(mas[0]);
     qsort(mas, n, sizeof(*mas), compare_name);
     for (size_t i = 0;i < n;i++)
     {
-        printf("name: %s age: %d \n", mas[i].name, mas[i].age);
+        printf("name: %s age: %d \n", mas[i]->name, mas[i]->age);
     }
     cout << endl;
     qsort(mas, n, sizeof(*mas), compare_age);
     for (size_t i = 0;i < n;i++)
     {
-        printf("name: %s age: %d \n", mas[i].name, mas[i].age);
+        printf("name: %s age: %d \n", mas[i]->name, mas[i]->age);
     }
-
+    for (auto i : mas)
+        delete i;
 
       return 0;
 }
